70efba5f45c110ddd0b9c0af0271d18d37c2b7a8
sherdorus <sherdor@ymail.com>
2025-05-25 23:39:44 +0500

    Add task status change and status display with showStatus method and mainStarter updates

--- src/todo/Helper.java
+++ src/todo/Helper.java
@@ -1,5 +1,7 @@
 package todo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -22,6 +24,9 @@ public class Helper {
                     case 2:
                         showTask();
                         break;
+                    case 3:
+                        changeTaskStatus();
+                        break;
                     default:
                         System.out.println("Invalid choice, please try again.");
                 }
@@ -43,7 +48,111 @@ public class Helper {
         System.out.print("Choose option: ");
     }
 
-    private static void addTask() {}
+    private static void emptyListMessage() {
+        System.out.println("There are no tasks in the list. Add a new task!");
+    }
 
-    private static void showTask() {}
+    private static void addTask() {
+        boolean loopStatus = true;
+        System.out.println("-----------------------");
+        System.out.println("Enter task information:");
+        System.out.println("-----------------------");
+        String taskInput = scanner.nextLine();
+        if (taskInput.isEmpty()) {
+            emptyListMessage();
+        } else {
+            tasks.add(new Task(taskInput, LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"))));
+            System.out.println("Task '" + taskInput + "' added successfully!");
+            while (loopStatus) {
+                System.out.println("------------------------------------------");
+                System.out.println("Do you want to add more tasks? Type Y or N");
+                System.out.println("------------------------------------------");
+                String moreTask = scanner.nextLine().toUpperCase();
+                if (moreTask.equals("Y") || moreTask.equals("YES")) {
+                    System.out.print("Enter task information: ");
+                    taskInput = scanner.nextLine();
+                    tasks.add(new Task(taskInput, LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"))));
+                    System.out.println("Task '" + taskInput + "' added successfully!");
+                } else {
+                    System.out.println("Returning to main menu...");
+                    loopStatus = false;
+                }
+            }
+        }
+    }
+
+    private static void showTask() {
+        int count = 1;
+        if (tasks.isEmpty()) {
+            emptyListMessage();
+        } else {
+            for (Task task : tasks) {
+                System.out.println(count + ": " + task.toString());
+                count++;
+            }
+        }
+    }
+
+    private static void showStatus() {
+        int count = 1;
+        if (tasks.isEmpty()) {
+            emptyListMessage();
+        } else {
+            for (TaskStatus taskStatus : TaskStatus.values()) {
+                System.out.println(count + ": " + taskStatus.toString());
+                count++;
+            }
+        }
+    }
+
+    private static void changeTaskStatus() {
+        if (tasks.isEmpty()) {
+            emptyListMessage();
+        } else {
+            System.out.println("------------------------------------");
+            System.out.println("Choose a task to change it's status:");
+            System.out.println("------------------------------------");
+            showTask();
+            int chooseTask = scanner.nextInt();
+            if (chooseTask > 0 && chooseTask <= tasks.size()) {
+                System.out.println("--------------------------");
+                System.out.println("Choose a status to assign:");
+                System.out.println("--------------------------");
+                showStatus();
+                int chooseStatus = scanner.nextInt();
+                scanner.nextLine();
+                if (chooseStatus <= TaskStatus.values().length && chooseStatus > 0) {
+                    switch (chooseStatus) {
+                        case 1: {
+                            tasks.get(chooseTask - 1).setTaskStatus(TaskStatus.IN_QUEUE);
+                            showTask();
+                            break;
+                        }
+                        case 2: {
+                            tasks.get(chooseTask - 1).setTaskStatus(TaskStatus.COMPLETED);
+                            showTask();
+                            break;
+                        }
+                        case 3: {
+                            tasks.get(chooseTask - 1).setTaskStatus(TaskStatus.PROGRESS);
+                            showTask();
+                            break;
+                        }
+                        case 4: {
+                            tasks.get(chooseTask - 1).setTaskStatus(TaskStatus.STARRED);
+                            showTask();
+                            break;
+                        }
+                        default:
+                            System.out.println("Invalid choice, please try again.");
+                            break;
+                    }
+                } else {
+                    System.out.println("Invalid status number!");
+                }
+            } else {
+                System.out.println("Invalid task number!");
+            }
+        }
+    }
 }
